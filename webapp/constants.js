7bfbc508fbe14f153fffcec73813011d5526f03a
Mark Huang <48987021+amwhow@users.noreply.github.com>
2026-01-25 12:42:59 +1100

    Auto-select transcribe sample rate by network

--- webapp/constants.js
+++ webapp/constants.js
@@ -10,3 +10,14 @@ export const AGENT_TRANSLATION_TO_AGENT_VOLUME = 0.1;
 export const TRANSCRIBE_PARTIAL_RESULTS_STABILITY = ["low", "medium", "high"];
 
 export const AUDIO_FEEDBACK_FILE_PATH = "./assets/background_noise.wav";
+
+export const TRANSCRIBE_RETRY_MAX_ATTEMPTS = 3;
+export const TRANSCRIBE_RETRY_BASE_DELAY_MS = 500;
+export const TRANSCRIBE_RETRY_MAX_DELAY_MS = 5000;
+export const TRANSCRIBE_TARGET_SAMPLE_RATE = 16000;
+
+export const TRANSCRIBE_AUTO_SAMPLE_RATE_PRESETS = {
+  high: 16000,
+  medium: 12000,
+  low: 8000,
+};
